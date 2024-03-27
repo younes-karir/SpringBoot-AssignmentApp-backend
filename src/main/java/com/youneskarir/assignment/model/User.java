2c5f4e7beae88d8d92fb15abbdd73bba0751d67c
younes-karir <youneskarir@outlook.com>
2024-03-27 22:21:07 +0000

    creating user entity

---
+++ src/main/java/com/youneskarir/assignment/model/User.java
@@ -0,0 +1,77 @@
+package com.youneskarir.assignment.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private LocalDate date;
+    
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    
+    
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "assignmentId",
+            referencedColumnName = "assignmentId"
+    )
+    private List<Assignment> assignments;
+    
+    
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
