881611b2ff44f8bfda10510a3fc8a5b6e5fa487a
younes-karir <youneskarir@outlook.com>
2024-03-30 21:01:09 +0000

    handle APIS responses

---
+++ src/main/java/com/youneskarir/assignment/response/ResponseHandle.java
@@ -0,0 +1,19 @@
+package com.youneskarir.assignment.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseHandle {
+    
+    public static ResponseEntity<Object> build(String message, HttpStatus status, Object object)
+    {
+        Map<String,Object> data =  new HashMap<>();
+        data.put("message",message);
+        data.put("status",status);
+        if(object!=null) data.put("data",object);
+        return new ResponseEntity<Object>(data,status);
+    }
+}
