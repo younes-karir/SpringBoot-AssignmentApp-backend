6e6cb4aa4684a8c05309c33399ac44df2ddc800f
younes-karir <youneskarir@outlook.com>
2024-03-31 21:59:27 +0000

    Adding custom exceptions

---
+++ src/main/java/com/youneskarir/assignment/advice/exception/BadRequestException.java
@@ -0,0 +1,15 @@
+package com.youneskarir.assignment.advice.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BadRequestException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/com/youneskarir/assignment/advice/exception/ElementAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.youneskarir.assignment.advice.exception;
+
+public class ElementAlreadyExistException extends RuntimeException{
+    public ElementAlreadyExistException(String message) {
+        super(message);
+    }
+}
